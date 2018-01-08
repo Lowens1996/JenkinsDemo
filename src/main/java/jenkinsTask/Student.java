194fb325b4c09beebcc238775d0077507368d3c1
Lowens1996 <32193115+Lowens1996@users.noreply.github.com>
2018-01-08 13:11:25 +0000

    Update Student.java

--- src/main/java/jenkinsTask/Student.java
+++ src/main/java/jenkinsTask/Student.java
@@ -13,6 +13,7 @@ public class Student {
         if (attendance < 30) return AttendanceGrade.VERY_POOR;
         if (attendance < 70) return AttendanceGrade.AVERAGE;
         if (attendance < 90) return AttendanceGrade.GOOD;
-		return AttendanceGrade.VERY_GOOD;
+	else return AttendanceGrade.VERY_GOOD;
+		
     }
 }
